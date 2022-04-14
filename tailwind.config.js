1bc709411db5e56149708627594ae2f2fe746e6f
bash62 <maxime.bachelet@etudiant.univ-lr.fr>
2022-04-15 01:03:09 +0200

    Init Openbase

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./assets/components/**/*.vue",
+            "./templates/**/*.html.twig"
+],
+  theme: {
+    extend: {
+      colors: {
+        black: "#1B262C",
+        navy: "#0F4C75",
+        blue: "#3282B8",
+        winter: "#BBE1FA",
+        
+      }
+    },
+  },
+  plugins: [],
+}
